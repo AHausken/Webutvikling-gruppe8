f64f1136926352b0051f904a1d06268b20e82caa
Alexander Hausken <alexanderhausken@Alexanders-MacBook-Pro.local>
2020-11-03 23:00:26 +0100

    Made new index.html, now responsive. also added NAV to UU.html and linked it to index.html

---
+++ JS/sushi.js
@@ -0,0 +1,70 @@
+
+
+const getFoodCard = () => {
+
+    return foodArray = [
+        {
+            id: 1,
+            name: "Dry-fish",
+            imageSrc: "dry-fish.jpg",
+            price: 99,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 2,
+            name: "flying-fish" ,
+            imageSrc: "flyfish-sushi.jpg",
+            price: 120,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 3,
+            name: "salmon-maki" ,
+            imageSrc: "salamon-maki.jpg",
+            price: 79,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 4,
+            name: "Sumo-deluxe-combo",
+            imageSrc: "sumo_deluxe-combo-16_9.jpg",
+            price: 250,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 5,
+            name: "sushi-plate-big",
+            imageSrc: "sushi-plate-big.JPG",
+            price: 199,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 6,
+            name: "sushi-plate-small" ,
+            imageSrc: "sushi-plate-small.jpg",
+            price: 79,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 7,
+            name: "tempura-plate",
+            imageSrc: "tempura-plate.jpg",
+            price: 150,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        },
+
+        {
+            id: 8,
+            name: "tempura-prawn",
+            imageSrc: "tempura-prawn.jpg",
+            price: 105,
+            info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, quasi!"
+        }
+];
+}
